c8af12429cf05a0849d8ebbf01b73c688483bc85
RJackson <rob@rjackson.me>
2014-05-26 13:00:59 +0100

    Gevent'd check_matches (and renamed it to check_heroes. Also added a manage.py instead of run.py

--- app/scripts/check_challenges.py
+++ app/scripts/check_challenges.py
@@ -1,4 +1,3 @@
-#!/srv/www/10herochallenge.com/10herochallenge/bin/python
 """
 
 For each active challenge (and inactive - within a few hours tollerance), fetch the user's matches since
@@ -10,16 +9,14 @@ re-roll was done (should we?)
 """
 
 from app import steam, db
-from app.models import Challenge
+from app.models import Challenge, ChallengeHero
 from datetime import datetime, timedelta
 from calendar import timegm as to_timestamp
 
-# Get challenges that did not end 23:00 yesterday (so yesterday's and todays)
-end_before = datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(hours=1)
-challenges = Challenge.query.filter(Challenge.end_at >= end_before).all()
+import gevent
 
-print "Found {} challenges to check".format(len(challenges))
-for challenge in challenges:
+
+def handle_challenge(challenge):
     webapi_params = {
         "account_id": challenge.user_id,
         "date_min": to_timestamp((challenge.start_at - timedelta(hours=2)).utctimetuple()),
@@ -28,6 +25,7 @@ for challenge in challenges:
     }
 
     matches = steam.api.interface("IDOTA2Match_570").GetMatchHistory(**webapi_params).get("result")
+    return_challenge_heroes = []
 
     print "Found {} matches for {}".format(len(matches.get("matches")), challenge.user_id)
     for match in matches.get("matches"):
@@ -64,18 +62,26 @@ for challenge in challenges:
         if (match_details.get('radiant_win') and player_was_radiant) or \
                 (not match_details.get('radiant_win') and not player_was_radiant):
             print "User won game as challenge hero in match {}.".format(match.get('match_id'))
-            challenge_hero.completed = True
-            db.session.add(challenge_hero)
+            return_challenge_heroes.append(challenge_hero.id)
         else:
             print "User lost game as challenge hero in match {}.".format(match.get('match_id'))
 
-    # Commit this challenge's data
+    # Return this challenge's data
+    return return_challenge_heroes
+
+
+def check_challenges():
+    """ Get a list of challenges, then spawn gevent threads for all of them. :) """
+    # Get challenges that did not end 23:00 yesterday (so yesterday's and todays)
+    end_before = datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(hours=1)
+    challenges = Challenge.query.filter(Challenge.end_at >= end_before).all()
+
+    print "Found {} challenges to check".format(len(challenges))
+    jobs = [gevent.spawn(handle_challenge, challenge) for challenge in challenges]
+
+    gevent.wait(jobs)
+
+    for hero in ChallengeHero.query.filter(ChallengeHero in [ch for ch in [j.value for j in jobs]]).all():
+        hero.completed = True
+        db.session.add(hero)
     db.session.commit()
-
-
-
-
-
-
-
-
---
+++ manage.py
@@ -0,0 +1,28 @@
+"""
+Manager script :)
+
+Takes the following commands:
+- runserver
+- shell
+- init_db
+- check_matches
+"""
+
+from app import app as application, db
+from flask.ext.script import Manager
+from app.scripts.check_challenges import check_challenges as _check_challenges
+
+manager = Manager(application)
+
+@manager.command
+def init_db():
+    db.create_all()
+
+
+@manager.command
+def check_challenges():
+    _check_challenges()
+
+
+if __name__ == "__main__":
+    manager.run()
